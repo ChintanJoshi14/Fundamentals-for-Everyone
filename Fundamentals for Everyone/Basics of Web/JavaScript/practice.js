d2833083a77088afa3152beb3c1436e7c7bbd376
Chintan Joshi <71627585+ChintanJoshi14@users.noreply.github.com>
2022-01-16 21:15:54 +0530

    Add files via upload

---
+++ Fundamentals for Everyone/Basics of Web/JavaScript/practice.js
@@ -0,0 +1,48 @@
+// ================================
+// Practice-1
+// ================================
+
+// function addFavoriteBooks(bookName) {
+//   if(!bookName.includes("Great")) {
+//     favoriteBooks.push(bookName);
+//   }
+// }
+  
+//   function printFavoriteBooks() {
+//     console.log(`Favorite Books: ${favoriteBooks.length}`);
+//     for(var books of favoriteBooks) {
+//       console.log(books);
+//     }
+//   }
+
+//   var favoriteBooks = [];
+
+// addFavoriteBooks("This is Great");
+// addFavoriteBooks("This is Great");
+// addFavoriteBooks("This is great");
+// addFavoriteBooks("This is bad");
+// addFavoriteBooks("This is not bad");
+// printFavoriteBooks();
+
+// ==============================
+// Practice-2
+// ==============================
+
+// var date = new Date("09/14/2000");
+// console.log(date);
+// date.toUTCString();
+// console.log(date);
+
+
+//type conversion-coercion
+var num = 2;
+// console.log(num);
+var string = 3;
+console.log(num+string);
+
+
+
+
+
+
+  
